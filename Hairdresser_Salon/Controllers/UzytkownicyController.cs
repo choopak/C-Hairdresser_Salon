53b9c6bed954f573d6a1a7c6655a010177600ee3
Tomek22811 <tomek2281@o2.pl>
2016-03-09 15:05:48 +0100

    Dodanie bazy danych

---
+++ Hairdresser_Salon/Controllers/UzytkownicyController.cs
@@ -0,0 +1,22 @@
+﻿using Hairdresser_Salon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hairdresser_Salon.Controllers
+{
+    public class UzytkownicyController : Controller
+    {
+        Uzytkownicy _uzytkownicy;
+
+        // GET: Uzytkownicy
+        public ActionResult Index()
+        {
+            this._uzytkownicy = new Uzytkownicy(1);
+            
+            return View();
+        }
+    }
+}
